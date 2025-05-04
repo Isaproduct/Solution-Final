f56d60bc55d3b575963c80c0eb1dd10f42ad701f
Dimon8706 <a3720906@gmail.com>
2025-05-05 02:03:09 +0500

    Implement full Spider enemy logic

---
+++ entities/Spider.java
@@ -0,0 +1,30 @@
+package entities;
+
+import main.Geme;
+
+public class Spider extends Enemy {
+
+    private int attackBoxOffsetX;
+
+    public Spider() {
+
+    }
+
+    private void initAttackBox() {
+    }
+
+    public void update() {
+    }
+
+    private void updateAttackBox() {
+    }
+
+    private void updateBehavior() {
+    }
+
+    public int flipX() {
+    }
+
+    public int flipW() {
+    }
+}
