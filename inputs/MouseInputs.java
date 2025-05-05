a218241e59a7b339abcbb1296a21c9544edce101
Akzhol Aisabek <akzhol.aisabek@gmail.com>
2025-05-05 17:09:26 +0500

    player control

--- inputs/MouseInputs.java
+++ inputs/MouseInputs.java
@@ -1,4 +1,100 @@
 package inputs;
 
-public class MouseInputs {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import gamestates.Gamestate;
+import main.Gpanel;
+
+public class MouseInputs implements MouseListener,MouseMotionListener {
+    private Gpanel gpanel;
+    public MouseInputs(Gpanel gpanel)
+    {
+        this.gpanel=gpanel;
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        switch(Gamestate.state)
+        {
+            case MENU:
+                gpanel.getGeme().getMenu().mouseMoved(e);
+                break;
+            case PLAYING:
+                gpanel.getGeme().getPlaying().mouseMoved(e);
+                break;
+            case OPTIONS:
+                gpanel.getGeme().getGameOptions().mouseMoved(e);
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        switch(Gamestate.state)
+        {
+            case PLAYING:
+                gpanel.getGeme().getPlaying().mouseClicked(e);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        switch(Gamestate.state)
+        {
+            case MENU:
+                gpanel.getGeme().getMenu().mousePressed(e);
+                break;
+            case PLAYING:
+                gpanel.getGeme().getPlaying().mousePressed(e);
+                break;
+            case OPTIONS:
+                gpanel.getGeme().getGameOptions().mousePressed(e);
+            default:
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        switch(Gamestate.state)
+        {
+            case MENU:
+                gpanel.getGeme().getMenu().mouseReleased(e);
+                break;
+            case PLAYING:
+                gpanel.getGeme().getPlaying().mouseReleased(e);
+                break;
+            case OPTIONS:
+                gpanel.getGeme().getGameOptions().mouseReleased(e);
+            default:
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
 }
