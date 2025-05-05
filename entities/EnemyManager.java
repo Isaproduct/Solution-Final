eb6374212b0ec7967b8ba15aef6fceac0ecaad8f
wwansterww <ansar.iken@narxoz.kz>
2025-05-05 21:13:39 +0500

    We decided to change the enemy from a spider to a crab

--- entities/EnemyManager.java
+++ entities/EnemyManager.java
@@ -13,19 +13,19 @@ import static utilz.Constants.EnemyConstants.*;
 public class EnemyManager {
     private Playing playing;
     private BufferedImage[][] crabbyArr;
-    private ArrayList<Spider> crabbies=new ArrayList<>();
+    private ArrayList<Crabby> crabbies=new ArrayList<>();
     public EnemyManager(Playing playing)
     {
         this.playing=playing;
         loadEnemyImgs();
     }
     public void LoadEnemies(Level level) {
-        spiders= level.getSpiders();
+        crabbies= level.getCrabs();
     }
     public void update(int[][] lvlData,Player player)
     {
         boolean isAnyActive=false;
-        for(Spider c:crabbies)
+        for(Crabby c:crabbies)
             if(c.isActive())
             {
                 c.update(lvlData, player);
@@ -36,23 +36,24 @@ public class EnemyManager {
     }
     public void draw(Graphics g,int xLvlOffset)
     {
-        drawSpiders(g,xLvlOffset);
+        drawCrabs(g,xLvlOffset);
 
     }
-    private void drawSpiders(Graphics g,int xLvlOffset) {
-        for(Spider c:spiders)
+    private void drawCrabs(Graphics g,int xLvlOffset) {
+        for(Crabby c:crabbies)
             if(c.isActive())
             {
-                g.drawImage(spiderArr[c.getEnemyState()][c.getAniIndex()],
-                        (int)(c.getHitbox().x-SPIDER_DRAWOFFSET_X)-xLvlOffset+c.flipX(),
-                        (int) (c.getHitbox().y-SPIDER_DRAWOFFSET_Y), SPIDER_WIDTH*c.flipW(), SPIDER_HEIGHT, null);
+                g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()],
+                        (int)(c.getHitbox().x-CRABBY_DRAWOFFSET_X)-xLvlOffset+c.flipX(),
+                        (int) (c.getHitbox().y-CRABBY_DRAWOFFSET_Y), CRABBY_WIDTH*c.flipW(), CRABBY_HEIGHT, null);
                 //c.drawHitbox(g, xLvlOffset);
                 //c.drawAttackBox(g, xLvlOffset);
             }
     }
 
-    public void checkEnemyHit(Rectangle2D.Float attackBox) {
-        for(Spider c:crabbies)
+    public void checkEnemyHit(Rectangle2D.Float attackBox)
+    {
+        for(Crabby c:crabbies)
             if(c.getCurrentHealth()>0)
                 if(c.isActive())
                     if(attackBox.intersects(c.getHitbox()))
@@ -63,15 +64,15 @@ public class EnemyManager {
     }
 
     private void loadEnemyImgs() {
-        spiderArr=new BufferedImage[5][9];
-        BufferedImage temp=LoadSave.GetSpriteAtlas(LoadSave.SPIDER_SPRITE);
+        crabbyArr=new BufferedImage[5][9];
+        BufferedImage temp=LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
         for(int j=0;j<crabbyArr.length;j++)
             for(int i=0;i<crabbyArr[j].length;i++)
-                crabbyArr[j][i]=temp.getSubimage(i*SPIDER_WIDTH_DEFAULT, j*SPIDER_HEIGHT_DEFAULT, SPIDER_WIDTH_DEFAULT, SPIDER_HEIGHT_DEFAULT);
+                crabbyArr[j][i]=temp.getSubimage(i*CRABBY_WIDTH_DEFAULT, j*CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
 
     }
     public void resetAllEnemies() {
-        for(Spider c:spiders)
+        for(Crabby c:crabbies)
             c.resetEnemy();
 
     }
