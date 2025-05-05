678f1e4a6761f3d1328d8e5cb998ed0e7cc38af9
Isaproduct <islam.ilimbayev@narxoz.kz>
2025-05-05 17:19:48 +0500

    I done the level.java file

--- levels/Level.java
+++ levels/Level.java
@@ -48,4 +48,66 @@ public class Level {
         cannons=HelpMethods.GetCannon(img);
 
     }
+    private void createSpikes() {
+        spikes=HelpMethods.GetSpikes(img);
+    }
+    private void createContainers() {
+        containers=HelpMethods.GetContainer(img);
+
+    }
+    private void createPotions() {
+        potions=HelpMethods.GetPotions(img);
+
+    }
+    private void calcPlayerSpawn() {
+        playerSpawn=GetPlayerSpawn(img);
+
+    }
+    private void calcLvlOffsets() {
+
+        lvlTilesWide=img.getWidth();
+        maxTilesOffset=lvlTilesWide-Geme.TILES_IN_WIDTH;
+        maxLvlOffsetx=Geme.TILES_SIZE*maxTilesOffset;
+    }
+    private void createEnemies() {
+        crabs=GetCrabs(img);
+
+    }
+    private void createLevelData() {
+
+        lvlData=GetLevelData(img);
+    }
+    public int getSpriteIndex(int x,int y)
+    {
+        return lvlData[y][x];
+    }
+
+    public int[][] getLevelData()
+    {
+        return lvlData;
+    }
+    public int getLvlOffset()
+    {
+        return maxLvlOffsetx;
+    }
+    public ArrayList<Crabby> getCrabs()
+    {
+        return crabs;
+    }
+    public Point getPlayerSpawn()
+    {
+        return playerSpawn;
+    }
+    public ArrayList<Potion> getPotions() {
+        return potions;
+    }
+    public ArrayList<GameContainer> getContainers() {
+        return containers;
+    }
+    public ArrayList<Spike> getSpikes() {
+        return spikes;
+    }
+    public ArrayList<Cannon> getCannons() {
+        return cannons;
+    }
 }
